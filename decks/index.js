eca3a0bab496370483d3422f2f8b197236ad198b
tommasofacchin <tommasofacchin77@gmail.com>
2026-01-09 22:02:03 +0100

    commit
    Co-authored-by: Elisa Perozzo <elisaperozzo@gmail.com>

--- decks/index.js
+++ decks/index.js
@@ -1,5 +1,4 @@
 // decks/index.js
 window.DECK_INDEX = [
-  { id: "topik1", label: "TOPIK 1", file: "decks/topik1.csv" },
-  { id: "hsk1", label: "HSK 1", file: "decks/hsk1.csv" }
+  { id: "hsk1", label: "HSK 1", data: window.HSK1_DECK }
 ];
